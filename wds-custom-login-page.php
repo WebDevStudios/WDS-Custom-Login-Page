7fae5d348387abd57bea325e13df4abade731b7c
Chris Reynolds <chris@jazzsequence.com>
2015-04-01 16:27:36 -0600

    check that cmb2 exists

--- wds-custom-login-page.php
+++ wds-custom-login-page.php
@@ -34,6 +34,9 @@ if ( ! class_exists( 'WDS_Custom_Login_Page' ) ) {
 			// Activation/Deactivation Hooks
 			register_activation_hook( __FILE__, array( $this, 'activate' ) );
 			register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+			// Make sure we have our requirements, and disable the plugin if we do not have them.
+			add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
 		}
 
 		/**
@@ -66,6 +69,37 @@ if ( ! class_exists( 'WDS_Custom_Login_Page' ) ) {
 		 */
 		public function deactivate() {
 
+		}
+
+		/**
+		 * Check that all plugin requirements are met
+		 *
+		 * @return boolean
+		 */
+		public static function meets_requirements() {
+			// Make sure we have CMB so we can use it
+			if ( ! defined( 'CMB2_LOADED' ) ) {
+				return false;
+			}
+
+			// We have met all requirements
+			return true;
+		}
+
+		/**
+		 * Check if the plugin meets requirements and
+		 * disable it if they are not present.
+		 */
+		public function maybe_disable_plugin() {
+			if ( ! $this->meets_requirements() ) {
+				// Display our error
+				echo '<div id="message" class="error">';
+				echo '<p>' . sprintf( __( 'WDS Simple Page Builder requires CMB2 but could not find it. The plugin has been <a href="%s">deactivated</a>. Please make sure all requirements are available.', 'wds-simple-page-builder' ), admin_url( 'plugins.php' ) ) . '</p>';
+				echo '</div>';
+
+				// Deactivate our plugin
+				deactivate_plugins( $this->basename );
+			}
 		}
 
 		/**
